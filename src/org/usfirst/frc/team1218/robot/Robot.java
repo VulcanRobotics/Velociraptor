20aad9f347b5e4a54417b7a735a43333e116d34d
Jinyu Liu <electronrush@gmail.com>
2018-02-03 11:26:47 -0500

    Added Elevator Encoder Readout and Logging to Elevator

--- src/org/usfirst/frc/team1218/robot/Robot.java
+++ src/org/usfirst/frc/team1218/robot/Robot.java
@@ -179,6 +179,7 @@ public class Robot extends TimedRobot {
 	
 	public void periodicTasks() {
 		driveTrain.periodicTasks();
+		elevator.periodicTasks();
 		
 	}
 }
--- src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
@@ -1,29 +1,32 @@
 package org.usfirst.frc.team1218.robot.subsystems;
 
+import org.usfirst.frc.team1218.robot.LoggableSRX;
 import org.usfirst.frc.team1218.robot.RobotMap;
 import org.usfirst.frc.team1218.robot.commands.elevator.ElevatorDefault;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Elevator extends Subsystem {
 	
-	static final boolean invertElevator = false;
+	static final boolean invertElevator = true;
 	static final boolean invertIntake = false;
 
 	boolean intakeStatus = false;
-	TalonSRX[] elevatorMotors = new TalonSRX[2];
+	LoggableSRX[] elevatorMotors = new LoggableSRX[2];
 	TalonSRX[] intakeMotors = new TalonSRX[2];
 	Solenoid intakeSolenoid;
 	Solenoid armSolenoid;
 	
 	public Elevator() {
 		for(int i = 0; i < 2; i ++) {
-			elevatorMotors[i] = new TalonSRX(RobotMap.elevatorMotorIds[i]);
+			elevatorMotors[i] = new LoggableSRX(RobotMap.elevatorMotorIds[i]);
 			elevatorMotors[i].setInverted(invertElevator);
 			elevatorMotors[i].enableVoltageCompensation(true);
 			
@@ -37,9 +40,20 @@ public class Elevator extends Subsystem {
 			intakeMotors[i].set(ControlMode.Follower, RobotMap.intakeMotorIds[0]);
 		}
 		
+		elevatorMotors[0].configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+		elevatorMotors[0].setSensorPhase(false);
+		
 		intakeSolenoid = new Solenoid(0);
 		armSolenoid = new Solenoid(1);
 		
+	}
+	
+	public void startLogging() {
+		elevatorMotors[0].startLogging();
+	}
+	
+	public void stopLogging() {
+		elevatorMotors[0].stopLogging();
 	}
 	
 	public void setElevatorPower(double elevatorPower) {
@@ -54,6 +68,10 @@ public class Elevator extends Subsystem {
 	}
 	public void armSolenoidEngage(boolean armState) {
 		armSolenoid.set(armState);
+	}
+	
+	public void periodicTasks() {
+		SmartDashboard.putString("DB/String 5", "Pe:" + elevatorMotors[0].getSelectedSensorPosition(0));
 	}
 	
 	@Override
