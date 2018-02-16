a0d08eb982d3ca51828eedefb75f0bb10a46ed8b
Ron Rossbach <rossbach.ron@gmail.com>
2018-02-16 16:42:59 -0500

    WIP

--- src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
@@ -7,10 +7,16 @@ import org.usfirst.frc.team1218.robot.commands.elevator.ElevatorDefault;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.Faults;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+<<<<<<< Upstream, based on master
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+=======
+import com.ctre.phoenix.motorcontrol.StatusFrame;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+>>>>>>> b3bc8ce WIP
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -23,10 +29,23 @@ public class Elevator extends Subsystem {
 
 	boolean intakeStatus = false;
 	boolean isLogging = false;
+	static final double[] elevatorGains = {0.0, 0.0, 0.0, 0.0};  //PIDF for Motion Magic
+	static final int elevatorCruiseVelocity = 0;		// encoder ticks / 100ms
+	static final int elevatorAcceleration = 0;			// change in verlocity per sec, i.e. when accel = cruise, will take 1sec to reach cruise
 	LoggableSRX[] elevatorMotors = new LoggableSRX[2];
 	TalonSRX[] intakeMotors = new TalonSRX[2];
 	Solenoid intakeSolenoid;
 	Solenoid armSolenoid;
+	
+	private Notifier processMPBuffer = new Notifier(new Runnable() {
+
+		@Override
+		public void run() {
+			elevatorMotors[0].processMotionProfileBuffer();
+			
+		}
+		
+	});
 	
 	public Elevator() {
 		for(int i = 0; i < 2; i ++) {
@@ -50,6 +69,16 @@ public class Elevator extends Subsystem {
 		elevatorMotors[0].configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
 		elevatorMotors[0].configForwardSoftLimitThreshold(275000, 0);
 		elevatorMotors[0].configForwardSoftLimitEnable(true, 0);
+		
+		elevatorMotors[0].config_kP(0, elevatorGains[0], 0);
+		elevatorMotors[0].config_kI(0, elevatorGains[1], 0);
+		elevatorMotors[0].config_kD(0, elevatorGains[2], 0);
+		elevatorMotors[0].config_kF(0, elevatorGains[2], 0);
+		
+		elevatorMotors[0].setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 10, 0);
+		elevatorMotors[0].setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 10, 0);
+		
+		elevatorMotors[0].configMotionCruiseVelocity(elevatorCruiseVelocity, 0);
 		
 		intakeSolenoid = new Solenoid(3);
 		armSolenoid = new Solenoid(2);
