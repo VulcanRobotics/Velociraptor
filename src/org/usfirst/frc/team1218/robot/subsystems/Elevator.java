4214853f3c7a62c195aab6ba5431ee8f11e24d43
Ron Rossbach <rossbach.ron@gmail.com>
2018-02-16 23:35:02 -0500

    constants now handled as properties, to allow same code for Beta and Alpha

--- src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1218/robot/subsystems/Elevator.java
@@ -24,14 +24,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator extends Subsystem {
 	
+<<<<<<< Upstream, based on master
 	static final boolean invertElevator = false;
 	static final boolean[] invertIntake = {true,false};
+=======
+//	static final boolean invertElevator = true;
+//	static final boolean[] invertIntake = {true,false};
+>>>>>>> 3276c0f constants now handled as properties, to allow same code for Beta and Alpha
 
 	boolean intakeStatus = false;
 	boolean isLogging = false;
-	static final double[] elevatorGains = {0.0, 0.0, 0.0, 0.0};  //PIDF for Motion Magic
-	static final int elevatorCruiseVelocity = 0;		// encoder ticks / 100ms
-	static final int elevatorAcceleration = 0;			// change in verlocity per sec, i.e. when accel = cruise, will take 1sec to reach cruise
+//	static final double[] elevatorGains = {0.0, 0.0, 0.0, 0.0};  //PIDF for Motion Magic
+//	static final int elevatorCruiseVelocity = 0;		// encoder ticks / 100ms
+//	static final int elevatorAcceleration = 0;			// change in verlocity per sec, i.e. when accel = cruise, will take 1sec to reach cruise
 	LoggableSRX[] elevatorMotors = new LoggableSRX[2];
 	TalonSRX[] intakeMotors = new TalonSRX[2];
 	Solenoid intakeSolenoid;
@@ -41,8 +46,7 @@ public class Elevator extends Subsystem {
 
 		@Override
 		public void run() {
-			elevatorMotors[0].processMotionProfileBuffer();
-			
+			elevatorMotors[0].processMotionProfileBuffer();		
 		}
 		
 	});
@@ -50,11 +54,11 @@ public class Elevator extends Subsystem {
 	public Elevator() {
 		for(int i = 0; i < 2; i ++) {
 			elevatorMotors[i] = new LoggableSRX(RobotMap.elevatorMotorIds[i]);
-			elevatorMotors[i].setInverted(invertElevator);
+			elevatorMotors[i].setInverted(RobotMap.elevatorMotorInvert);
 			elevatorMotors[i].enableVoltageCompensation(true);
 			
 			intakeMotors[i] = new TalonSRX(RobotMap.intakeMotorIds[i]);
-			intakeMotors[i].setInverted(invertIntake[i]);
+			intakeMotors[i].setInverted(RobotMap.intakeMotorInvert[i]);
 			intakeMotors[i].enableVoltageCompensation(true);
 		}
 		
@@ -70,18 +74,19 @@ public class Elevator extends Subsystem {
 		elevatorMotors[0].configForwardSoftLimitThreshold(275000, 0);
 		elevatorMotors[0].configForwardSoftLimitEnable(true, 0);
 		
-		elevatorMotors[0].config_kP(0, elevatorGains[0], 0);
-		elevatorMotors[0].config_kI(0, elevatorGains[1], 0);
-		elevatorMotors[0].config_kD(0, elevatorGains[2], 0);
-		elevatorMotors[0].config_kF(0, elevatorGains[2], 0);
+		elevatorMotors[0].config_kP(0, RobotMap.elevatorPIDF[0], 0);
+		elevatorMotors[0].config_kI(0, RobotMap.elevatorPIDF[1], 0);
+		elevatorMotors[0].config_kD(0, RobotMap.elevatorPIDF[2], 0);
+		elevatorMotors[0].config_kF(0, RobotMap.elevatorPIDF[3], 0);
 		
 		elevatorMotors[0].setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 10, 0);
 		elevatorMotors[0].setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 10, 0);
 		
-		elevatorMotors[0].configMotionCruiseVelocity(elevatorCruiseVelocity, 0);
+		elevatorMotors[0].configMotionCruiseVelocity(RobotMap.elevatorCruiseVelocity, 0);
+		elevatorMotors[0].configMotionAcceleration(RobotMap.elevatorAcceleration, 0);
 		
-		intakeSolenoid = new Solenoid(3);
-		armSolenoid = new Solenoid(2);
+		intakeSolenoid = new Solenoid(RobotMap.intakePort);
+		armSolenoid = new Solenoid(RobotMap.armPort);
 		
 	}
 	
