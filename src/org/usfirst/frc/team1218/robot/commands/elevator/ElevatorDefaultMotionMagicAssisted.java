b2a35e88d8ced839434c0643133337740d83759b
Jinyu Liu <electronrush@gmail.com>
2018-02-25 15:42:43 -0500

    Fixed bug where elevator return to bottom after exiting moveToPoint

--- src/org/usfirst/frc/team1218/robot/commands/elevator/ElevatorDefaultMotionMagicAssisted.java
+++ src/org/usfirst/frc/team1218/robot/commands/elevator/ElevatorDefaultMotionMagicAssisted.java
@@ -23,6 +23,7 @@ public class ElevatorDefaultMotionMagicAssisted extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    		setpoint = Robot.elevator.getCurrentPosition();
     }
 
     // Called repeatedly when this Command is scheduled to run
